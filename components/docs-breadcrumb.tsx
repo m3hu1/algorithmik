f97d6dab5c827f4e95040b69a7a85c47f8e7c795
Mehul Pathak <pathakkmehul@gmail.com>
2024-11-18 12:49:10 +0530

    added links to breadcrumb

--- components/docs-breadcrumb.tsx
+++ components/docs-breadcrumb.tsx
@@ -6,6 +6,7 @@ import {
     BreadcrumbPage,
     BreadcrumbSeparator,
   } from "@/components/ui/breadcrumb";
+import Link from "next/link";
   import { Fragment } from "react";
   
   export default function DocsBreadcrumb({ paths }: { paths: string[] }) {
@@ -14,24 +15,28 @@ import {
         <Breadcrumb>
           <BreadcrumbList>
             <BreadcrumbItem>
-              <BreadcrumbLink>Guide</BreadcrumbLink>
+              <BreadcrumbLink asChild>
+              <Link href="/guide/introduction">Guide</Link></BreadcrumbLink>
             </BreadcrumbItem>
-            {paths.map((path, index) => (
+            {paths.map((path, index) => {
+            // Construct the href by joining all paths up to current index
+            const href = `/guide/${paths.slice(0, index + 1).join("/")}`;
+            
+            return (
               <Fragment key={path}>
                 <BreadcrumbSeparator />
                 <BreadcrumbItem>
                   {index < paths.length - 1 ? (
-                    <BreadcrumbLink className="a">
-                      {toTitleCase(path)}
+                    <BreadcrumbLink asChild>
+                      <Link href={href}>{toTitleCase(path)}</Link>
                     </BreadcrumbLink>
                   ) : (
-                    <BreadcrumbPage className="b">
-                      {toTitleCase(path)}
-                    </BreadcrumbPage>
+                    <BreadcrumbPage>{toTitleCase(path)}</BreadcrumbPage>
                   )}
                 </BreadcrumbItem>
               </Fragment>
-            ))}
+            );
+          })}
           </BreadcrumbList>
         </Breadcrumb>
       </div>
