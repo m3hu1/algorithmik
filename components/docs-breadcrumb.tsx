885d84014dfdd9cf58cf5a888a518bbb32caeada
Mehul Pathak <pathakkmehul@gmail.com>
2024-12-23 23:16:40 +0530

    refactored breadcrumbs code

--- components/docs-breadcrumb.tsx
+++ components/docs-breadcrumb.tsx
@@ -1,27 +1,28 @@
 import {
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbList,
-    BreadcrumbPage,
-    BreadcrumbSeparator,
-  } from "@/components/ui/breadcrumb";
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import Link from "next/link";
-  import { Fragment } from "react";
-  
-  export default function DocsBreadcrumb({ paths }: { paths: string[] }) {
-    return (
-      <div className="pb-5">
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink asChild>
-              <Link href="/guide/introduction">Guide</Link></BreadcrumbLink>
-            </BreadcrumbItem>
-            {paths.map((path, index) => {
+import { Fragment } from "react";
+
+export default function DocsBreadcrumb({ paths }: { paths: string[] }) {
+  return (
+    <div className="pb-5">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/guide/introduction">Guide</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          {paths.map((path, index) => {
             // Construct the href by joining all paths up to current index
             const href = `/guide/${paths.slice(0, index + 1).join("/")}`;
-            
+
             return (
               <Fragment key={path}>
                 <BreadcrumbSeparator />
@@ -37,17 +38,16 @@ import Link from "next/link";
               </Fragment>
             );
           })}
-          </BreadcrumbList>
-        </Breadcrumb>
-      </div>
-    );
-  }
-  
-  function toTitleCase(input: string): string {
-    const words = input.split("-");
-    const capitalizedWords = words.map(
-      (word) => word.charAt(0).toUpperCase() + word.slice(1)
-    );
-    return capitalizedWords.join(" ");
-  }
-  
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+}
+
+function toTitleCase(input: string): string {
+  const words = input.split("-");
+  const capitalizedWords = words.map(
+    (word) => word.charAt(0).toUpperCase() + word.slice(1),
+  );
+  return capitalizedWords.join(" ");
+}
