5db7ba618cf6b4e4be2328b6b5f01617c8e0748b
Mehul Pathak <pathakkmehul@gmail.com>
2025-01-19 00:09:55 +0530

    added telegram community link

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -27,6 +27,7 @@ import AnimatedUnderline from "@/components/markdown/underline";
 import ProblemProgress from "@/components/markdown/problem-progress";
 import AlertComponent from "@/components/markdown/alert";
 import dynamic from "next/dynamic";
+import { Telegram } from "iconoir-react";
 
 // add custom components
 const components = {
@@ -51,6 +52,7 @@ const components = {
   SubmitProblemSetForm: dynamic(
     () => import("@/components/SubmitProblemSetForm"),
   ),
+  Telegram,
 };
 
 // can be used for other pages like blogs, Guides etc
